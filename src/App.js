766329cdf7cceafb8deb6ed0ec3cb7c09ed5193d
John-Xu1 <XuJohn400@gmail.com>
2023-05-16 16:14:00 -0700

    basic chat completion

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [prompt, setPrompt] = useState('');
+
+  async function completePrompt(input) {
+    const response = await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            role: "user",
+            content: input,
+          }
+        ]
+      }),
+    });
+    const data = await response.json();
+    console.log(data);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <input onChange={(e) => setPrompt(e.target.value)}></input>
+        <button onClick={() => {completePrompt(prompt)}}>Click to complete</button>
       </header>
     </div>
   );
