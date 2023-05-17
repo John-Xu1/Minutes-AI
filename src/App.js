71cad42b6f4ed22281401cd295dbff9dc4d01572
John-Xu1 <XuJohn400@gmail.com>
2023-05-16 22:14:07 -0700

    frontend clone

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react';
+import "./App.css";
+import { useState } from "react";
+import { FaPlus } from "react-icons/fa";
+import { AiOutlineSend } from "react-icons/ai";
 // import { ChakraProvider, Box, Center, Input, Button } from '@chakra-ui/react';
 
 function App() {
-  const [prompt, setPrompt] = useState('');
+  const [prompt, setPrompt] = useState("");
 
   async function completePrompt(input) {
     const response = await fetch("https://api.openai.com/v1/chat/completions", {
@@ -19,8 +20,8 @@ function App() {
           {
             role: "user",
             content: input,
-          }
-        ]
+          },
+        ],
       }),
     });
     const data = await response.json();
@@ -28,13 +29,50 @@ function App() {
   }
 
   return (
-      <div className='app'>
-        <section className='sideBar'>
-          <button>New Chat</button>
-        </section>
-        <section className='main'></section>
-      </div>
-    
+    <div className="app">
+      <section className="sideBar">
+        <button id="newChatButton" onClick={() => {}}>
+          <p>
+            <FaPlus id="plusIcon" /> New Chat
+          </p>
+        </button>
+        <ul id="history">
+          <li>chat 1</li>
+          <li>chat 2</li>
+        </ul>
+        <div>
+          <hr />
+          <p>Made by John</p>
+        </div>
+      </section>
+      <section className="main">
+        <h1>XuPT</h1>
+        <ul id="conversation"></ul>
+        <div className="prompt">
+          <input
+            id="sendField"
+            type="text"
+            placeholder="Send a message."
+            onChange={(e) => {
+              setPrompt(e.target.value);
+            }}
+          />
+          <div id="sendButton">
+            <AiOutlineSend
+              id="sendIcon"
+              size={20}
+              onClick={() => {
+                let li = document.createElement("li");
+                let textNode = document.createTextNode(prompt);
+                li.appendChild(textNode);
+                document.getElementById("conversation").appendChild(li);
+                // completePrompt(prompt);
+              }}
+            />
+          </div>
+        </div>
+      </section>
+    </div>
   );
 }
 
